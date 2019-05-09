7087abce7d1b0dfd16bbc58771d0ec8bc36e0e3d
p3t3r-k <49058063+p3t3r-k@users.noreply.github.com>
2019-05-09 13:03:19 +0200

    destroyed CalenderActivity

--- app/src/main/java/com/example/hreminder/CalenderActivity.java
+++ app/src/main/java/com/example/hreminder/CalenderActivity.java
@@ -1,7 +1,12 @@
 package com.example.hreminder;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class CalenderActivity extends AppCompatActivity {
 
@@ -9,5 +14,31 @@ public class CalenderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
+        ActionBar abar = getActionBar();
+        abar.setLogo(R.drawable.calender);
+        abar.setDisplayUseLogoEnabled(true);
+        abar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuinfl = getMenuInflater();
+        menuinfl.inflate(R.menu.calender_ab, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.info_id:
+                Toast.makeText(getApplicationContext(), "Info icon is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.setting_id:
+                Toast.makeText(getApplicationContext(), "Setting icon is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
