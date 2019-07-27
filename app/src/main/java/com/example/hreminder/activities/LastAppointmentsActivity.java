7fd2df3e8ea39bc8be41b3a00a0e1bec0accfb32
knesslau17 <laura.kness@edu.fh-joanneum.at>
2019-07-27 10:47:21 +0200

    Calendar Activity Zeiträume + Table Layout

--- app/src/main/java/com/example/hreminder/activities/LastAppointmentsActivity.java
+++ app/src/main/java/com/example/hreminder/activities/LastAppointmentsActivity.java
@@ -82,7 +82,7 @@ public class LastAppointmentsActivity extends BaseActivity {
         super.onStart();
         iDUser = LastUser.getLastUserID();
 
-        if (db.getAppointmentsByID(iDUser)) {
+        if (db.checkIfAppointmentsExistByID(iDUser)) {
             rebuildTableLayout();
         }
     }
@@ -168,7 +168,7 @@ public class LastAppointmentsActivity extends BaseActivity {
     }
 
     private void addRow() {
-        tableLayout = findViewById(R.id.tableLayout);
+        tableLayout = findViewById(R.id.tableLayoutLastApp);
         row = new TableRow(this);
         TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT);
 
